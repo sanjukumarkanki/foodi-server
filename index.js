ac3a019a8d64fe9a5f2eea4de1fc79540eb2824b
K SANJU KUMAR <129849456+sanjukumarkanki@users.noreply.github.com>
2024-01-06 21:12:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-    success_url: "http://localhost:5173/cart-page",
-    cancel_url: "http://localhost:5173/cancel",
+    success_url: "/cart-page",
+    cancel_url: "/cancel",
   });
   res.json({ id: session.id });
 });
